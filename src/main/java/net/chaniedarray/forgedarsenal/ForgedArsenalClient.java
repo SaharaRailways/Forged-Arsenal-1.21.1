ac7e0fea85d9b59d20964520eb3a6073ee72d270
Chanied <132589948+WolfZoned@users.noreply.github.com>
2025-01-04 11:04:09 -0500

    Change Licence to GNU AGPL

---
+++ src/main/java/net/chaniedarray/forgedarsenal/ForgedArsenalClient.java
@@ -0,0 +1,10 @@
+package net.chaniedarray.forgedarsenal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForgedArsenalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chaniedarray/forgedarsenal/ForgedArsenalDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chaniedarray.forgedarsenal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ForgedArsenalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
