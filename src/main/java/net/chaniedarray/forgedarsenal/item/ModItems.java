5bf9ca77416e8be3388d15b100e9bafef7596a46
Chanied <132589948+WolfZoned@users.noreply.github.com>
2025-01-07 09:39:33 -0500

    made the modItems class

---
+++ src/main/java/net/chaniedarray/forgedarsenal/item/ModItems.java
@@ -0,0 +1,9 @@
+package net.chaniedarray.forgedarsenal.item;
+
+import net.chaniedarray.forgedarsenal.ForgedArsenal;
+
+public class ModItems {
+    public static void registerModItems() {
+        ForgedArsenal.LOGGER.info("Registering items");
+    }
+}
