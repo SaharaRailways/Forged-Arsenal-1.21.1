8280373311c90f85c84f86857e55c03b2097fbd4
Chanied <132589948+WolfZoned@users.noreply.github.com>
2025-01-19 15:05:42 -0500

    fixed a potential null pointer exception

--- src/main/java/net/chaniedarray/forgedarsenal/item/custom/ChiselItem.java
+++ src/main/java/net/chaniedarray/forgedarsenal/item/custom/ChiselItem.java
@@ -18,6 +18,7 @@ import net.minecraft.world.World;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class ChiselItem extends Item {
     private static final Map<Block, Block> CHISEL_MAP =
@@ -40,7 +41,7 @@ public class ChiselItem extends Item {
             if(!world.isClient()) {
                 world.setBlockState(context.getBlockPos(), CHISEL_MAP.get(clickedBlock).getDefaultState());
                 context.getStack().damage(1, ((ServerWorld) world), ((ServerPlayerEntity) context.getPlayer()),
-                        item -> context.getPlayer().sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
+                        item -> Objects.requireNonNull(context.getPlayer()).sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
 
                 world.playSound(null, context.getBlockPos(), SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS);
             }
